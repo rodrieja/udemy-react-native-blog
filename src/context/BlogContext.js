96b11a7cc1e01cba710d1984a6813c0dcdb7e1d5
Alejandro Rodriguez <aerodriguez@BFNB14928s-MacBook-Pro.local>
2020-11-08 19:49:30 -0300

    Adding Post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
-export const BlogProvider = ({children}) => {
-    return <BlogContext.Provider>
+export const BlogProvider = ({ children }) => {
+    const [blogPost, setBlogPost] = useState([])
+    const addBlogPost = () => {
+        setBlogPost([...blogPost, { title: `Blog Post #${blogPost.length + 1}` }]);
+    }
+
+    return <BlogContext.Provider value={{ data: blogPost, addBlogPost }}>
         {children}
     </BlogContext.Provider>
 }
+
+export default BlogContext;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+    const { data, addBlogPost } = useContext(BlogContext)
+
     return (
         <View>
             <Text>IndexScreen</Text>
+            <Button title='Add Post' onPress={addBlogPost} />
+            <FlatList
+                data={data}
+                keyExtractor={(blog) => blog.title}
+                renderItem={({ item }) => {
+                    return <Text>{item.title}</Text>
+                }}
+            />
         </View>
     )
 }
